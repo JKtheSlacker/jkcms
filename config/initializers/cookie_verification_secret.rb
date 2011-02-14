be833af092e8c6aa3d59902deccb90929c73a81d
JK Wood <joshuakwood@gmail.com>
2011-02-14 14:39:28 -0600

    Fresh Rails app in accordance with the prophecy

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '5d02e8d8d25cf66272cc91a67217864c53b14f6f77b22571451637edaee728df6f3b32bbdbe0e205ca9b78c0e4bb206110ed9c8adeb94577e7078778c8bf8b32';
